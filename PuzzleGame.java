68acfc6f4aa9c0350951fffda28f85af2a9a8ad2
sasharebello <sasharebello19@gmail.com>
2021-11-11 13:04:25 +0530

    background

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-
-public class PuzzleGame{
+import java.awt.event.*;
+public class PuzzleGame extends Frame implements ActionListener{
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
+    private JTextField textField;
+    private JMenuBar menuBar;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,bcheat,bcheat2;
     PuzzleGame(){
-        JFrame f= new JFrame("Puzzle Game");
-        f.setSize(255,400);
-        f.setBackground(Color.WHITE);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JMenuBar mb= new JMenuBar();
+        frame= new JFrame("Puzzle Game");
+        frame.setSize(255,400);
+        frame.setBackground(Color.WHITE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel=new JPanel();
+        panel.setBackground(Color.WHITE);
+        menuBar= new JMenuBar();
         JMenu m1= new JMenu("Home");
         JMenu m2= new JMenu("Rules");
-        mb.add(m1);
-        mb.add(m2);
-        f.add(mb);
-        f.getContentPane().add(BorderLayout.NORTH,mb);
-        Button b1,b2,b3,b4,b5,b6,b7,b8,b9,bcheat,bcheat2;
+        menuBar.add(m1);
+        menuBar.add(m2);
+        frame.add(menuBar);
+        frame.getContentPane().add(BorderLayout.NORTH,menuBar);
+        
         b1=new Button("1");
         b1.setBounds(50,100,40,40);
         b2=new Button("2");
@@ -35,35 +43,195 @@ public class PuzzleGame{
         b9.setBounds(150,200,40,40);
         bcheat=new Button("");
         bcheat.setBounds(150,200,40,40);
-        JPanel p= new JPanel();
         JButton bstr= new JButton("Start");
         bstr.setBounds(40, 300, 70, 25);
         JButton bres= new JButton("Reset");
         bres.setBounds(130, 300 ,70, 25);
         bcheat2=new Button("");
         bcheat2.setBounds(150,200,40,40);
-        f.add(b1);
-        f.add(b2);
-        f.add(b3);
-        f.add(b4);
-        f.add(b5);
-        f.add(b6);
-        f.add(b7);
-        f.add(b8);
-        f.add(b9);
-        f.add(bcheat);
-        f.add(bstr);
-        f.add(bres);
-        f.add(bcheat2);
-        f.getContentPane().add(BorderLayout.SOUTH,p);
+
+        b1.addActionListener(this);  
+        b2.addActionListener(this);  
+        b3.addActionListener(this);  
+        b4.addActionListener(this);  
+        b5.addActionListener(this);  
+        b6.addActionListener(this);  
+        b7.addActionListener(this);  
+        b8.addActionListener(this);  
+        b9.addActionListener(this);
+
+        frame.add(b1);
+        frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
+        frame.add(b5);
+        frame.add(b6);
+        frame.add(b7);
+        frame.add(b8);
+        frame.add(b9);
+        frame.add(bcheat);
+        frame.add(bstr);
+        frame.add(bres);
+        frame.add(bcheat2);
+        frame.getContentPane().add(BorderLayout.CENTER,panel);
         bcheat.setVisible(false);
         bcheat.setEnabled(false);
         bcheat2.setVisible(false);
         bcheat2.setEnabled(false);
-        f.setVisible(true);
+ 
+        frame.setVisible(true);
     }
-    
-    public static void main(String args[]){
+public void LevelChoice() {
+        Frame f = new Frame("Program for exp 14");    
+        Choice c = new Choice();   
+  
+        c.setBounds(100, 100, 75, 75);    
+        c.add("Tea");    
+        c.add("Coffee");    
+        c.add("Juice");    
+        c.add("Cola");    
+        c.add("Water");    
+
+        f.add(c);    
+
+        f.setSize(400, 400);    
+        f.setLayout(null);    
+        f.setVisible(true); 
+}
+
+public void actionPerformed(ActionEvent e){  
+    if(e.getSource()==b1){  
+        String label=b1.getLabel();  
+        if(b2.getLabel().equals("")){  
+            b2.setLabel(label);  
+            b1.setLabel("");  
+        }  
+        if(b4.getLabel().equals("")){  
+            b4.setLabel(label);  
+            b1.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b2){  
+        String label=b2.getLabel();  
+        if(b1.getLabel().equals("")){  
+            b1.setLabel(label);  
+            b2.setLabel("");  
+        }  
+        if(b3.getLabel().equals("")){  
+            b3.setLabel(label);  
+            b2.setLabel("");  
+        }  
+        if(b5.getLabel().equals("")){  
+            b5.setLabel(label);  
+            b2.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b3){  
+        String label=b3.getLabel();  
+        if(b2.getLabel().equals("")){  
+            b2.setLabel(label);  
+            b3.setLabel("");  
+        }  
+        if(b6.getLabel().equals("")){  
+            b6.setLabel(label);  
+            b3.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b4){  
+        String label=b4.getLabel();  
+        if(b1.getLabel().equals("")){  
+            b1.setLabel(label);  
+            b4.setLabel("");  
+        }  
+        if(b7.getLabel().equals("")){  
+            b7.setLabel(label);  
+            b4.setLabel("");  
+        }  
+        if(b5.getLabel().equals("")){  
+            b5.setLabel(label);  
+            b4.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b5){  
+        String label=b5.getLabel();  
+        if(b2.getLabel().equals("")){  
+            b2.setLabel(label);  
+            b5.setLabel("");  
+        }  
+        if(b6.getLabel().equals("")){  
+            b6.setLabel(label);  
+            b5.setLabel("");  
+        }  
+        if(b4.getLabel().equals("")){  
+            b4.setLabel(label);  
+            b5.setLabel("");  
+        }  
+        if(b8.getLabel().equals("")){  
+            b8.setLabel(label);  
+            b5.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b6){  
+        String label=b6.getLabel();  
+        if(b9.getLabel().equals("")){  
+            b9.setLabel(label);  
+            b6.setLabel("");  
+        }  
+        if(b3.getLabel().equals("")){  
+            b3.setLabel(label);  
+            b6.setLabel("");  
+        }  
+        if(b5.getLabel().equals("")){  
+            b5.setLabel(label);  
+            b6.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b7){  
+        String label=b7.getLabel();  
+        if(b4.getLabel().equals("")){  
+            b4.setLabel(label);  
+            b7.setLabel("");  
+        }  
+        if(b8.getLabel().equals("")){  
+            b8.setLabel(label);  
+            b7.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b8){  
+        String label=b8.getLabel();  
+        if(b9.getLabel().equals("")){  
+            b9.setLabel(label);  
+            b8.setLabel("");  
+        }  
+        if(b7.getLabel().equals("")){  
+            b7.setLabel(label);  
+            b8.setLabel("");  
+        }  
+        if(b5.getLabel().equals("")){  
+            b5.setLabel(label);  
+            b8.setLabel("");  
+        }  
+    }  
+    if(e.getSource()==b9){  
+        String label=b9.getLabel();  
+        if(b6.getLabel().equals("")){  
+            b6.setLabel(label);  
+            b9.setLabel("");  
+        }  
+        if(b8.getLabel().equals("")){  
+            b8.setLabel(label);  
+            b9.setLabel("");  
+        }  
+    } 
+    //congrats code  
+    if(b1.getLabel().equals("1")&&b2.getLabel().equals("2")&&b3.getLabel()    
+        .equals("3")&&b4.getLabel().equals("4")&&b5.getLabel().equals("5")    
+            &&b6.getLabel().equals("6")&&b7.getLabel().equals("7")&&b8.getLabel()    
+        .equals("8")&&b9.getLabel().equals("")){     
+            JOptionPane.showMessageDialog(this,"Congratulations! You won.");    
+        }    
+}  
+public static void main(String args[]){
         new PuzzleGame();
     }
 }
